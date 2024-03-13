087db03000f15a12f1f967db7907da41a070aa84
sadikmahmudadive <siradive137@gmail.com>
2024-03-14 03:01:04 +0600

    no change

--- quickSort_057.cpp
+++ quickSort_057.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include<iostream>
 using namespace std;
 void Swap(int arr[], int s, int e)
@@ -48,55 +47,4 @@ int main()
         cout<<arr[i]<<" ";
     }
     return 0;
-=======
-#include<iostream>
-using namespace std;
-void Swap(int arr[], int s, int e)
-{
-    int temp=arr[s];
-    arr[s]=arr[e];
-    arr[e]=temp;
-}
-int Partition(int arr[], int s, int e)
-{
-    int pivot= arr[s];
-    int i=s,j=e;
-    while(i<j)
-    {
-       do
-        {
-            i++;
-        }
-        while(arr[i]<=pivot);
-        do
-        {
-            j--;
-        }
-        while(arr[j]>pivot);
-        if(i<j)
-            Swap(arr,i,j);
-    }
-    Swap(arr,s,j);
-    return j;
-}
-void quicksort(int arr[],int s, int e)
-{
-    if(s<e)
-    {
-        int pivot=Partition(arr,s,e);
-        quicksort(arr,s,pivot);
-        quicksort(arr,pivot+1,e);
-    }
-}
-int main()
-{
-    int arr[]={3,5,2,6,4,7};
-    int n=sizeof(arr)/sizeof(arr[0]);
-    quicksort(arr,0,n-1);
-    for(int i=0;i<n;i++)
-    {
-        cout<<arr[i]<<" ";
-    }
-    return 0;
->>>>>>> 59f02f6c7ef4248ba7d85964ef2a53455d58ad43
 }
