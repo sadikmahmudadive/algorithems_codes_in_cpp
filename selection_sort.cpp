722fa892e4a2091a1d305d9e845e535408dcfc2c
sadikmahmudadive <siradive137@gmail.com>
2024-03-14 02:46:22 +0600

    no change

--- selection_sort.cpp
+++ selection_sort.cpp
@@ -1,6 +1,4 @@
-<<<<<<< HEAD
 #include <iostream>
-
 using namespace std;
 
 // Selection Sort function
@@ -34,40 +32,3 @@ int main()
 
     return 0;
 }
-=======
-#include <iostream>
-
-using namespace std;
-
-// Selection Sort function
-void selectionSort(int arr[], int n) 
-{
-    for (int i = 0; i < n - 1; i++) 
-    {
-        int minIndex = i;
-        for (int j = i + 1; j < n; j++) 
-        {
-            if (arr[j] < arr[minIndex])
-                minIndex = j;
-        }
-        // Swap arr[i] and arr[minIndex]
-        int temp = arr[i];
-        arr[i] = arr[minIndex];
-        arr[minIndex] = temp;
-    }
-}
-
-int main() 
-{
-    int arr[] = {64, 25, 12, 22, 11};
-    int n = sizeof(arr) / sizeof(arr[0]);
-
-    selectionSort(arr, n);
-
-    cout << "Sorted array: ";
-    for (int i = 0; i < n; i++)
-        cout << arr[i] << " ";
-
-    return 0;
-}
->>>>>>> 59f02f6c7ef4248ba7d85964ef2a53455d58ad43
